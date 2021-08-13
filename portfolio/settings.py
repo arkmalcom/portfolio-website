e19041e52a8639ffc7af45325b0662202308f282
Malcom Calivar <arkmalcom@gmail.com>
2021-08-13 15:55:32 -0400

    imagefield testing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = '/home/arkmalcom/portfolio-website/static'
+MEDIA_ROOT = '/home/arkmalcom/portfolio-website/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
