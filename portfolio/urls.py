c8b675c2fd517e5bdf87c7afc2f688faaf1a8abd
Malcom Calivar <arkmalcom@gmail.com>
2021-08-14 16:20:20 -0400

    deploy full site

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
     path('projects/', include('projects.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
