e022fc1b31a802277348cbfad8a2283363472b96
Malcom Calivar <46433723+arkmalcom@users.noreply.github.com>
2021-08-15 21:56:54 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r&hvq=lk-$^1o+gwaixi00u*l#23$96n%$zwdv!hz8hp3&x+t@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -135,8 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email configuration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'overlordarkard@gmail.com'
-EMAIL_HOST_PASSWORD = 'sR0tZam0RastA'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
